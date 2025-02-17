b95c46d1d508754e24951c0102bd8cb1c912f040
Vishnu-Royal-07 <onlyraithu1@gmail.com>
2025-02-17 22:27:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkout() {
+    alert("Proceeding to checkout!");
+}
+
+function login() {
+    alert("Redirecting to login page.");
+}
+
+function register() {
+    alert("Redirecting to registration page.");
+}
